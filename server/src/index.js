08ffeef02a5d5033d87e9636f5f9f7d28d0aa85c
Raag Bhutani <bhutani2002@gmail.com>
2023-11-05 12:39:36 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -39,6 +39,7 @@ const cors = require("cors");
 const bodyParser = require("body-parser");
 const { formRouter } = require("./routes/form");
 const app = express();
+app.use(bodyParser.json());
 
 app.use(cors({
   origin: "https://deploy-mern-travel-buddies.vercel.app",
@@ -46,7 +47,13 @@ app.use(cors({
   methods: ["GET", "POST", "PUT"], // Specify the allowed HTTP methods
 }));
 
-app.use(bodyParser.json());
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin: *'); // Allow requests from any origin
+  res.header('Access-Control-Allow-Methods: GET, POST, OPTIONS, PUT, DELETE'); // Allow the specified methods
+  res.header('Access-Control-Allow-Headers: Origin, X-Requested-With, Content-Type, Accept'); // Allow the specified headers
+  next();
+});
+
 app.use("/api/v1/", formRouter);
 
 app.listen(process.env.PORT, () => {
